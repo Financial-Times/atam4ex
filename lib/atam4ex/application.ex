9661902037a2e633a129ee311ab998532a959314
Ellis Pritchard <ellis.pritchard@ft.com>
2017-09-12 13:05:05 +0100

    default opts in Application stub

--- lib/atam4ex/application.ex
+++ lib/atam4ex/application.ex
@@ -11,14 +11,14 @@ defmodule ATAM4Ex.Application do
   end
   ```
 
-  Your module will now be a standard OTP application which can be 
+  Your module will now be a standard OTP application which can be
   started and stopped, and will start (and stop) the tests running
   on a regular basis.
 
-  Then add application configuration to your `mix.exs` to start  
+  Then add application configuration to your `mix.exs` to start
   your module as an application on VM start-up:
 
-  ```  
+  ```
   def application do
     [
       extra_applications: [:logger],
@@ -28,10 +28,10 @@ defmodule ATAM4Ex.Application do
   ```
 
   ATAM4Ex will look for configuration under the `:atam4ex` key
-  of the application of the 'using' module, or the `otp_app` key, 
-  but you get the chance to modify this configuration by implementing 
-  the `atam4ex_opts/1` function, wherein you can replace or modify 
-  any options before returning them, e.g. you might provide them from 
+  of the application of the 'using' module, or the `otp_app` key,
+  but you get the chance to modify this configuration by implementing
+  the `atam4ex_opts/1` function, wherein you can replace or modify
+  any options before returning them, e.g. you might provide them from
   another source, or replace place-holders.
 
     ```
@@ -44,14 +44,14 @@ defmodule ATAM4Ex.Application do
   end
   ```
   """
-  
+
   defmacro __using__(opts) do
     quote do
       use Application
 
       def start(_type, _args) do
         app = unquote(opts)[:otp_app] || Application.get_application(__MODULE__)
-        opts = Application.get_env(app, :atam4ex)
+        opts = Application.get_env(app, :atam4ex) || []
         opts = atam4ex_opts(opts)
 
         config = ATAM4Ex.init(opts)
@@ -61,7 +61,7 @@ defmodule ATAM4Ex.Application do
       def atam4ex_opts(opts) do
         opts
       end
-  
+
       defoverridable [atam4ex_opts: 1]
     end
   end
