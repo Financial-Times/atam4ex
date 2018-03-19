12020f381c2f01a0104fc42f12efe3ddc87d3966
Ellis Pritchard <ellis.pritchard@ft.com>
2018-03-19 16:13:13 +0000

    correct project version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ATAM4Ex.Mixfile do
     [
       app: :atam4ex,
       version: "0.1.1",
-      elixir: "~> 1.5 or ~> 1.6",
+      elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
