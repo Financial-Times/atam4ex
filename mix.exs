ce56f5c0a60eaf08b05ddc4be0c734642605ff4d
Ellis Pritchard <ellis.pritchard@ft.com>
2018-03-19 16:11:53 +0000

    bump versions, deps

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ATAM4Ex.Mixfile do
   def project do
     [
       app: :atam4ex,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.1.1",
+      elixir: "~> 1.5 or ~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
